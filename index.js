b06cc84aa448b846183244c01c8b27cfa48445ed
manoj-patial <dev.cs.patial@gmail.com>
2025-09-01 23:24:37 +0530

    second commit

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 var a = 1;
 var message = "Hello World this is the first message" ;
+
+
+// 8706e36 (HEAD -> main, origin/main) first commit 
+
+var message = "Adding new change and check the new commit" ;
