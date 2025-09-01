86972c244668b9074ff9ff06a0b800b9c6cd0a16
manoj-patial <dev.cs.patial@gmail.com>
2025-09-01 23:27:46 +0530

    fourth commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var a = 1;
 var message = "Hello World this is the first message" ;
 
-
+// git log --oneline 
 // 8706e36 (HEAD -> main, origin/main) first commit 
 
 var message = "Adding new change and check the new commit" ;
@@ -14,3 +14,8 @@ function add(a, b) {
 }
 
 console.log(add(1, 2));
+
+// c2b8ae8 (HEAD -> main, origin/main) third commit
+
+// using git diff we ccan see the current and old changes in the file if we got a lot of changes
+// let do 4 th commit
