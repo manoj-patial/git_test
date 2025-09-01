2fb15ff03c0374cba1913fc3dd3b2e99fe49de94
manoj-patial <dev.cs.patial@gmail.com>
2025-09-01 23:47:23 +0530

    New Sixth Commit

--- index.js
+++ index.js
@@ -23,3 +23,15 @@ console.log(add(1, 2));
 
 // git show <commit-hash> to see the changes in the commit
 // for examople git show c2b8ae8 ,, will show me the changes in this commit , which is the third commit
+
+// now as i reverted this in local , so my origin branch is 2 commit ahead 
+// since i want my origina repo also get reverted to this commit 
+// i need to push the chagnes using git push origin main --force
+
+        // git log --oneline           
+        // 75ac70c (HEAD -> main, origin/main) fifth commit
+        // 86972c2 fourth commit
+        // c2b8ae8 third commit
+        // b06cc84 second commit
+
+// Lets add the new commit now 
