6509aa47f99100c07dbeefb65c5e76b1ee0b05a7
manoj-patial <dev.cs.patial@gmail.com>
2025-09-01 23:52:40 +0530

    2nd commit in new branch b1

--- b1.js
+++ b1.js
@@ -1,1 +1,25 @@
 var msg = "added new branch and let see the git graph now on commiting this "
+// so now we can see the grpah like 
+// commit 08059f96f00f8c4ec6731ee3039f144af70b1ef9 (HEAD -> b1, origin/b1)
+// Author: manoj-patial <dev.cs.patial@gmail.com>
+// Date:   Mon Sep 1 23:49:43 2025 +0530
+
+//     new b1.js added in branch b1
+
+// commit 2fb15ff03c0374cba1913fc3dd3b2e99fe49de94 (origin/main, main)
+// Author: manoj-patial <dev.cs.patial@gmail.com>
+// Date:   Mon Sep 1 23:47:23 2025 +0530
+
+//     New Sixth Commit
+
+// commit 75ac70c4e7f0552d1fb46e573e88e9ff3a960efe
+// Author: manoj-patial <dev.cs.patial@gmail.com>
+// Date:   Mon Sep 1 23:35:16 2025 +0530
+
+//     fifth commit
+
+// commit 86972c244668b9074ff9ff06a0b800b9c6cd0a16
+// Author: manoj-patial <dev.cs.patial@gmail.com>
+// Date:   Mon Sep 1 23:27:46 2025 +0530
+
+// lets add one more commit here
